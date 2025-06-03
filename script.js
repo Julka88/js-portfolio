6070b51d28731705e52b313772be87b0ee910ef6
Glitch (glitch-hello-website) <none>
2025-06-03 17:22:58 +0000

    🍼🚲 Checkpoint ./script.js:75938034/6923 ./index.html:75938034/181

--- script.js
+++ script.js
@@ -1,152 +1,150 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
-burger.addEventListener("click",()=>{
-    navLinks.classList.toggle("active");
-})
-
-
-
-gsap.to(".name",{
-    text:"I am a web developer.",
-    duration:3,
-    repeat:3,
-    repeatDelay: .8,
-    ease:"power1.in",
-    yoyo:true,
-    delay:5,
-
-
-})
-
-
-
-gsap.from('.img-one', {x:10, duration: 10, delay: 4,rotation:360, ease: 'power2.in', opacity: 0.5, })
-
-
-
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 180,
-      "density": {
-        "enable": true,
-        "value_area": 1000
-      }
-    },
-    "color": {
-      "value": "#73DE4C"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 5,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
+gsap.to(".name", {
+  text: "I am a web developer.",
+  duration: 3,
+  repeat: 3,
+  repeatDelay: 0.8,
+  ease: "power1.in",
+  yoyo: true,
+  delay: 5,
+});
+
+gsap.from(".img-one", {
+  x: 10,
+  duration: 10,
+  delay: 4,
+  rotation: 360,
+  ease: "power2.in",
+  opacity: 0.5,
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 180,
+      density: {
+        enable: true,
+        value_area: 1000,
+      },
+    },
+    color: {
+      value: "#73DE4C",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 140,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
 
 /* ---- stats.js config ---- */
 
 var count_particles, stats, update;
-stats = new Stats;
+stats = new Stats();
 stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
 document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
   stats.begin();
   stats.end();
   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
@@ -156,25 +154,21 @@ update = function() {
 };
 requestAnimationFrame(update);
 
+const wave = document.getElementById("animatedWave");
 
+let t = 0;
+function animateWave() {
+  t += 0.02;
+  const amplitude = 50;
+  const control1 = 360 + Math.sin(t) * amplitude;
+  const control2 = 1080 - Math.sin(t) * amplitude;
 
-  const wave = document.getElementById("animatedWave");
+  const newPath = `M0,0 C${control1},100 ${control2},0 1440,100 L1440,100 L0,100 Z`;
+  wave.setAttribute("d", newPath);
 
-  let t = 0;
-  function animateWave() {
-    t += 0.02;
-    const amplitude = 50;
-    const control1 = 360 + Math.sin(t) * amplitude;
-    const control2 = 1080 - Math.sin(t) * amplitude;
+  requestAnimationFrame(animateWave);
+}
 
-    const newPath = `M0,0 C${control1},100 ${control2},0 1440,100 L1440,100 L0,100 Z`;
-    wave.setAttribute("d", newPath);
+animateWave();
 
-    requestAnimationFrame(animateWave);
-  }
-
-  animateWave();
-
-  gsap.to(".footer-heard",{
-
-  })
+gsap.to(".footer-heard", {});
